c404a99e5b5619af117fb21091315a8d3b34e210
MatheusSC017 <matheussimao2101@gmail.com>
2024-10-19 09:54:05 -0300

    Creating class to generate images to logos and post images

---
+++ Core/images.py
@@ -0,0 +1,48 @@
+from openai import OpenAI
+import dotenv
+import os
+
+from Utils.patterns import singleton
+
+dotenv.load_dotenv()
+
+
+class Dalle:
+    _client = None
+
+    def __init__(self, model="dall-e-3", size="1024x1024", quality="standard"):
+        self.model = model
+        self.size = size
+        self.quality = quality
+
+    @property
+    def client(self):
+        if self._client is None:
+            self._set_client()
+        return self._client
+
+    def _set_client(self):
+        api_key = os.environ.get('OPENAI_KEY')
+        self._client = OpenAIUnique(api_key=api_key)
+
+    def send_request(self, prompt):
+        response = self.client.images.generate(
+          model=self.model,
+          prompt=prompt,
+          size=self.size,
+          quality=self.quality,
+          n=1,
+        )
+
+        return response.data[0].url
+
+
+@singleton
+class OpenAIUnique(OpenAI):
+    pass
+
+
+if __name__ == "__main__":
+    instance = Dalle()
+
+    print(instance.send_request("Logo para empresa de Energia Solar com as inicias NMV (Novo Mundo Verde)"))
