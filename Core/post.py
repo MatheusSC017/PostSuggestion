e09c1422718a54efe4728f7d7d15c601c0b90bed
MatheusSC017 <matheussimao2101@gmail.com>
2024-11-03 07:57:28 -0300

    Fixing Generating post code

--- Core/post.py
+++ Core/post.py
@@ -10,15 +10,14 @@ class PostSuggestAssistant(ChatGPT):
         super().__init__(*args, **kwargs)
 
         self.messages[0]["content"] = (
-            "You are a helpful assistant that suggests 3 examples of posts per request for "
-            "social media-oriented posts based on user-provided characteristics, posts must"
-            " be enclosed in double quotes."
+            "You are a helpful assistant that suggests 3 examples of posts per request for social media-oriented posts "
+            "based on user-provided characteristics, posts must be enclosed in double quotes. You must follow the "
+            "required number of characters (Size), language and number of emojis allowed"
         )
 
     def get_suggestions(self, product_characteristics="", **kwargs):
         for key, value in kwargs.items():
-            if key in self.basic_configs.keys():
-                self.basic_configs[key] = value
+            self.basic_configs[key] = value
         if product_characteristics:
             self.send_request(product_characteristics)
         return self.suggestions
