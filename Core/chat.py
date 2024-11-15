ac3ecd8170689ca77cff64463e1f880aa68d99bf
MatheusSC017 <matheussimao2101@gmail.com>
2024-11-15 10:18:36 -0300

    Including chat to get tips and strategies about social media

---
+++ Core/chat.py
@@ -0,0 +1,26 @@
+import json
+
+from Core.base import ChatGPT
+
+
+class Chat(ChatGPT):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.messages[0]["content"] = (
+            "You are a helpful assistant that will help with questions relationed to social media, example: posts, "
+            "events, timelines and others"
+        )
+
+    def send_request(self, message):
+        self.messages.append({"role": "user", "content": message})
+
+        response = self.client.chat.completions.create(
+            model=self.model, messages=self.messages, temperature=0
+        )
+
+        response = json.load(response.model_dump_json())["choices"][0]["message"][
+            "content"
+        ]
+        self.messages.append({"role": "assistant", "content": response})
+        return response
