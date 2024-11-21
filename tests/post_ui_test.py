e3c1b83643e5c0fd9851b63fd6997c815b1c8e8b
MatheusSC017 <matheussimao2101@gmail.com>
2024-11-21 07:52:56 -0300

    fixing test to post generation method

--- tests/post_ui_test.py
+++ tests/post_ui_test.py
@@ -1,7 +1,6 @@
 from unittest.mock import MagicMock
 
 import pytest
-from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication
 
 from ui.post import GeneratePostUI
@@ -16,14 +15,24 @@ def qt_app():
 
 @pytest.fixture
 def fake_assistant():
-    fake_client = MagicMock()
-    fake_client.send_request.return_value = ["Suggestion 1", "Suggestion 2", "Suggestion 3"]
-    return fake_client
+    def _fake_assistant(content):
+        fake_client = MagicMock()
+        fake_client.send_request.return_value = content
+        return fake_client
+
+    return _fake_assistant
 
 
 @pytest.fixture
 def generate_post_ui(qt_app, fake_assistant):
-    return GeneratePostUI(suggestions=[], test_client=fake_assistant)
+    assistant_instance = fake_assistant(
+        [
+            "Suggestion 1",
+            "Suggestion 2",
+            "Suggestion 3",
+        ]
+    )
+    return GeneratePostUI(suggestions=[], test_client=assistant_instance)
 
 
 def test_initial_ui_state(generate_post_ui):
@@ -33,16 +42,13 @@ def test_initial_ui_state(generate_post_ui):
     assert generate_post_ui.language_cb.currentText() == "English"
 
 
-# def test_post_generation(generate_post_ui, qtbot):
-#     generate_post_ui.post_content_edit.setText("Original text to generate the post")
-#     assert len(generate_post_ui.suggestions) == 0
-#     qtbot.mouseClick(generate_post_ui.generate_posts_button, Qt.MouseButton.LeftButton)
-#     qtbot.wait(1000)
-#     assert len(generate_post_ui.suggestions) == 3
-#
-#     assert generate_post_ui.suggestions[0] == "Suggestion 1"
-#     assert generate_post_ui.suggestions[1] == "Suggestion 2"
-#     assert generate_post_ui.suggestions[2] == "Suggestion 3"
-#
-#     generate_post_ui.close()
-#     del generate_post_ui
+def test_post_generation(generate_post_ui):
+    generate_post_ui.show()
+    generate_post_ui.post_content_edit.setText("Original text to generate the post")
+    assert len(generate_post_ui.suggestions) == 0
+    generate_post_ui.generate_posts_button.click()
+    assert len(generate_post_ui.suggestions) == 3
+
+    assert generate_post_ui.suggestions[0] == "Suggestion 1"
+    assert generate_post_ui.suggestions[1] == "Suggestion 2"
+    assert generate_post_ui.suggestions[2] == "Suggestion 3"
